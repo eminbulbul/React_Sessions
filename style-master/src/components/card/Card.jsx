844e89651a32ade0fad943a57db7989d156a640d
m_emin <mehmeteminbul@gmail.com>
2022-03-22 00:12:33 +0300

    style sessions ended

--- style-master/src/components/card/Card.jsx
+++ style-master/src/components/card/Card.jsx
@@ -1,10 +1,25 @@
 import "./Card.scss";
 
-const Card = ({veri}) => {
-
+const Card = ({ veri }) => {
   return (
-    <div>Card</div>
-  )
-}
+    <div className="container">
+      {veri.map((eleman) => {
+        //destructuring
+        const { id, countries, capital, img, population } = eleman;
+        return (
+          <div className="card" key={id}>
+            <h1>{countries}</h1>
+            <img src={img} />
+            <h2> {capital}</h2>
+            <h4>{population}</h4>
 
-export default Card
+            <button className="card--small">Small</button>
+            <button className="card--large">Large</button>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Card;
