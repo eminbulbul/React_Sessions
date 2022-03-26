90d528e83618ae85c0181fc01be994ea44066170
m_emin <mehmeteminbul@gmail.com>
2022-03-26 13:01:32 +0300

    session 1 ended

--- rc05_lifecycle_useEffect/src/App.js
+++ rc05_lifecycle_useEffect/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import ClassComponent from './components/ClassComponent'
 
 function App() {
   return (
     <div className="App">
-      <h2>LifeCycles</h2>
+      {/* <h2>LifeCycles</h2> */}
+      <ClassComponent />
     </div>
   )
 }
