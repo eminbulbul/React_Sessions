2371f6752ed0dc91b45136528e31bea54ab5373b
m_emin <mehmeteminbul@gmail.com>
2022-03-26 15:52:57 +0300

    Session Ended

--- rc05_lifecycle_useEffect/src/App.js
+++ rc05_lifecycle_useEffect/src/App.js
@@ -1,14 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ClassComponent from './components/ClassComponent'
-import { useState } from 'react'
+import UseEffectComp from './components/UseEffectComp'
+import Users from './components/Users'
+import UseStateComp from './components/UseStateComp'
 
 function App() {
   const [isVisible, setIsVisible] = useState(true)
   return (
     <div className="App">
       {/* <h2>LifeCycles</h2> */}
-      <button onClick={() => setIsVisible(!isVisible)}>Toggle Comp</button>
-      {isVisible && <ClassComponent />}
+      {/* <button onClick={() => setIsVisible(!isVisible)}>Toggle Comp</button> */}
+      {/* {isVisible && <ClassComponent />} */}
+      {/* <UseStateComp /> */}
+      {/* {isVisible && <UseEffectComp />} */}
+      <Users />
     </div>
   )
 }
--- rc05_lifecycle_useEffect/src/components/UseStateComp.js
+++ rc05_lifecycle_useEffect/src/components/UseStateComp.js
@@ -1,9 +1,32 @@
 import React from 'react'
-
+import { useState } from 'react'
+//! Don’t call Hooks inside loops, conditions, or nested functions.
+//! Don’t call Hooks from regular JavaScript functions.
+//* Only call Hooks at the top level
+//* Call Hooks from React function components.
+//* Call Hooks from custom Hooks.
 const UseStateComp = () => {
+  const [count, setCount] = useState(0)
+  const [age, setAge] = useState(4)
+  const [user, setUser] = useState({ name: 'Emin', age: '25', email: 'emin@gmail.com' })
+
+  // const increase = () => setCount(count + 1)
+  const increase = () => setCount((count) => count + 1)
+  const increaseAge = () => setAge(age + 1)
+  const updateUser = () => setUser({ ...user, name: 'Emin' })
+  console.log(user)
+
   return (
     <div className="function">
       <h2>Functional Component</h2>
+      <p>Count: {count} </p>
+      <button onClick={increase}>Increase Count</button>
+      <p>Age: {age} </p>
+      <button onClick={increaseAge}>Increase Age</button>
+      <p>Name: {user.name} </p>
+      <p>Age: {user.age} </p>
+      <p>Email: {user.email} </p>
+      <button onClick={updateUser}>Update User</button>
     </div>
   )
 }
