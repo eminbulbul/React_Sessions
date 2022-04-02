087549a9356b63130f0f820a11b205c247f39b24
m_emin <mehmeteminbul@gmail.com>
2022-04-02 16:46:41 +0300

    products builded

--- rc08_router/router-class/src/App.js
+++ rc08_router/router-class/src/App.js
@@ -4,6 +4,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Products from "./pages/Products";
+import Details from "./pages/Details";
+import NotFound from "./components/NotFound";
 import Footer from "./components/Footer";
 
 function App() {
@@ -11,11 +13,15 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/products" element={<Products />} />
-        </Routes>
+        <div className="main">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/products" element={<Products />} />
+            <Route path="/details" element={<Details />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </div>
         <Footer />
       </BrowserRouter>
     </div>
