9d072030c11eb2a812145266f7564ce561cd0ce3
m_emin <mehmeteminbul@gmail.com>
2022-04-04 21:17:10 +0300

    will back later

--- rc08_router/router-class/src/pages/Details.js
+++ rc08_router/router-class/src/pages/Details.js
@@ -1,13 +1,33 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import styles from "./Detail.module.css";
 
 const Details = () => {
   const params = useParams();
-  fetch(`https://fakestoreapi.com/products/${params.productId}`)
-    .then((res) => res.json())
-    .then((json) => console.log(json));
-
-  return <div>Details</div>;
+  const [product, setProduct] = useState({});
+  useEffect(() => {
+    fetch(`https://fakestoreapi.com/products/${params.productId}`)
+      .then((res) => res.json())
+      .then((json) => setProduct(json));
+  }, []);
+  return (
+    <div>
+      <h1>Details Page</h1>
+      <div className={styles.detailsWrapper}>
+        <p>{product?.id}</p>
+        <p>{product?.title}</p>
+        <p>{product?.desciption}</p>
+        <p>{product?.category}</p>
+        <p>{product?.price}</p>
+        <p>{product?.rating?.rate}</p>
+        <p>{product?.rating?.count}</p>
+        <img
+          className={styles.detailsImage}
+          src={product?.image}
+          alt={product?.title}
+        />
+      </div>
+    </div>
+  );
 };
-
 export default Details;
