6120fe9f8d5806710308bf0c98d62feae78630bf
m_emin <mehmeteminbul@gmail.com>
2022-04-09 16:16:48 +0300

    with context

--- rc09_context/src/App.js
+++ rc09_context/src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
-import WithPropsApp from "./withprops/WithPropsApp";
+// import WithPropsApp from "./withprops/WithPropsApp";
+import WithContextApp from "./withcontext/WithContextApp";
 
 function App() {
   return (
     <div className="App">
       <h2>useContext Class</h2>
-      <WithPropsApp />
+      {/* <WithPropsApp /> */}
+      <WithContextApp />
     </div>
   );
 }
