7577fce57a239c27943241ac4def1b60262f1137
m_emin <mehmeteminbul@gmail.com>
2022-04-14 22:01:20 +0300

    conter reducer and reducer ended session 1 ended

--- redux-class-10-master/src/components/counter/Counter.js
+++ redux-class-10-master/src/components/counter/Counter.js
@@ -1,18 +1,20 @@
 import "./Counter.css";
+import { useSelector, useDispatch } from "react-redux";
 
 const Counter = () => {
-  
+  const { counter } = useSelector((state) => state.counterReducer);
+
   return (
     <div className="app">
       <h2 className="counter-header">Counter With Redux</h2>
       <h1
-        // className={
-        //   counter > 0
-        //     ? "counter-pos"
-        //     : counter < 0
-        //     ? "counter-neg"
-        //     : "counter-zero"
-        // }
+      // className={
+      //   counter > 0
+      //     ? "counter-pos"
+      //     : counter < 0
+      //     ? "counter-neg"
+      //     : "counter-zero"
+      // }
       >
         counter
       </h1>
---
+++ redux-class-10-master/src/redux/index.js
@@ -0,0 +1,12 @@
+import { createStore, combineReducers } from "redux";
+import counterReducer from "./reducer/counterReducer";
+
+const reducers = combineReducers({
+  counterReducer: counterReducer,
+  //   todoReducer: todoReducer,
+});
+
+export const combinedStores = () => {
+  const store = createStore(reducers);
+  return store;
+};
