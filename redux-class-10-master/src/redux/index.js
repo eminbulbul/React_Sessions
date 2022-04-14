f42360baaa37b5cf05ecc4dc0720e82736d0b2c9
m_emin <mehmeteminbul@gmail.com>
2022-04-15 00:12:45 +0300

    redux session ended

--- redux-class-10-master/src/redux/index.js
+++ redux-class-10-master/src/redux/index.js
@@ -1,12 +1,10 @@
 import { createStore, combineReducers } from "redux";
 import counterReducer from "./reducer/counterReducer";
+import todoReducer from "./reducer/todoReducer";
 
 const reducers = combineReducers({
   counterReducer: counterReducer,
-  //   todoReducer: todoReducer,
+  todoReducer : todoReducer
 });
 
-export const combinedStores = () => {
-  const store = createStore(reducers);
-  return store;
-};
+export const myStore = createStore(reducers);
